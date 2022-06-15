dffb5caaee538ba77e1eec0ceb6a7da1214d5fb7
drestew <drestew48@gmail.com>
2022-06-15 18:22:48 -0400

    Add quotes to db

--- server.js
+++ server.js
@@ -3,25 +3,52 @@ const express = require('express')
 const app = express()
 const MongoClient = require('mongodb').MongoClient
 
-app.listen(3000, function () {
-    console.log('listening on 3000')
-})
+// app.listen(3000, function () {
+//     console.log('listening on 3000')
+// })
 
-app.use(bodyParser.urlencoded({ extended: true }))
+// app.use(bodyParser.urlencoded({ extended: true }))
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html')
-})
+// app.get('/', (req, res) => {
+//     res.sendFile(__dirname + '/index.html')
+// })
 
-app.post('/quotes', (req, res) => {
-    console.log(req.body)
-})
+// app.post('/quotes', (req, res) => {
+//     console.log(req.body)
+// })
 
-async function connectDatabase() {
-    const connect = await MongoClient.connect('mongodb+srv://windu:starwars_crud@cluster0.qrptoto.mongodb.net/?retryWrites=true&w=majority')
-    const db = await connect.db('quotes_crud')
-    console.log('connected to database')
-    // if (err) return console.error(err)
-}
 
-connectDatabase()
+const connectionStr = 'mongodb+srv://windu:starwars_crud@cluster0.qrptoto.mongodb.net/?retryWrites=true&w=majority'
+MongoClient.connect(connectionStr)
+    .then(client => {
+        console.log('connected to database')
+        const db = client.db('quotes_crud')
+        const quotesCollection = db.collection('quotes')
+
+        app.use(bodyParser.urlencoded({ extended: true }))
+
+        app.get('/', (req, res) => {
+            res.sendFile(__dirname + '/index.html')
+            const cursor = db.collection('quotes').find().toArray()
+                .then(results => {
+                    console.log(results)
+                })
+                .catch(error => console.error(error))
+        })
+
+        app.post('/quotes', (req, res) => {
+            quotesCollection.insertOne(req.body)
+                .then(result => {
+                    console.log(result)
+                    res.redirect('/')
+                })
+                .catch(error => console.error(error))
+        })
+
+        app.listen(3000, function () {
+            console.log('listening on 3000')
+        })
+    })
+    .catch(error => console.error(error))
+
+
